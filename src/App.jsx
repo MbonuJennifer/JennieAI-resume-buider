1b2c797f3d67b1e1476c615a76b62acdb82dca17
MbonuJennifer <MbonuifeomaJennifer@gmail.com>
2024-07-12 12:23:08 +0100

    Implemented routes for pages.

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Button } from './components/ui/button'
+// import { Button } from './components/ui/button'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      Subscribe to Jennifer
-      <Button>Subscribe</Button>
+      <Outlet/>
     </>
   )
 }
